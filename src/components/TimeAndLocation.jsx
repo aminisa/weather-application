bdd6532c8897498de384ac58ab06e0592b37bb91
Shadan Amini <shadanamini@Shadans-MacBook-Pro.local>
2024-04-06 12:02:52 -0700

    created space between date and local time

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -4,8 +4,11 @@ function TimeAndLocation() {
   return (
     <div>
       <div className="flex items-center justify-center my-6">
-        <p className="text-white text-md font-extralight">
-          Tuesday, 31 May 2022 ~ Local Time: 12:46 PM
+        <p className="text-white text-md justify-between font-extralight">
+          Tuesday, 31 May 2022
+        </p>
+        <p className="text-white text-md justify-between font-extralight ml-20">
+          Local Time: 12:46 PM
         </p>
       </div>
       <div className="flex items-center justify-center my-3">
