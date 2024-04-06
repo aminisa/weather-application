006d2ca0c64818bb83bd1456305b00eddda287f9
Shadan Amini <shadanamini@Shadans-MacBook-Pro.local>
2024-04-06 11:57:15 -0700

    imported forecast component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import Forecast from "./components/Forecast";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Inputs />
       <TimeAndLocation />
       <TemperatureAndDetails />
+      <Forecast title="Hourly Forecast"/>
     </div>
   );
 }
